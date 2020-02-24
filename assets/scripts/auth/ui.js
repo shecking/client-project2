111a9c04006564bdf841928714e468d64e39bc27
Steven Hecking <shecking1115@gmail.com>
2020-02-24 16:45:30 -0500

    Adding basic success and failure handling
    Adding a displayed message in index.html.
    Adding basic bootstrap SCSS for success/failure colors in index.scss.
    Adding success and error messages in ui.js.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -5,34 +5,60 @@ const store = require('./../store')
 
 const onSignUpSuccessPST = function (response) {
   store.user = response.user
+  $('#message').removeClass()
+  $('#message').addClass('success-message')
+  $('#message').text(response.user.email + ' signed up successfully. Welcome to Practice Session Tracker!')
+  $('#sign-up').trigger('reset')
 }
 
 const onSignUpFailurePST = function (response) {
-
+  $('#message').removeClass()
+  $('#message').addClass('failure-message')
+  $('#message').text('Error: PST sign up failed. Please try again.')
+  $('#sign-up').trigger('reset')
 }
 
 const onSignInSuccessPST = function (response) {
   store.user = response.user
+  $('#message').removeClass()
+  $('#message').addClass('success-message')
+  $('#message').text(response.user.email + ' signed in to Practice Session Tracker.')
+  $('#sign-in').trigger('reset')
 }
 
 const onSignInFailurePST = function (response) {
-
+  $('#message').removeClass()
+  $('#message').addClass('failure-message')
+  $('#message').text('Error: PST sign in failed. Please try again.')
+  $('#sign-in').trigger('reset')
 }
 
 const onChangePasswordSuccessPST = function (response) {
-
+  $('#message').removeClass()
+  $('#message').addClass('success-message')
+  $('#message').text('Password changed successfully.')
+  $('#change-password').trigger('reset')
 }
 
 const onChangePasswordFailurePST = function (response) {
-
+  $('#message').removeClass()
+  $('#message').addClass('failure-message')
+  $('#message').text('Error: password not changed. Please try again.')
+  $('#change-password').trigger('reset')
 }
 
 const onSignOutSuccessPST = function (response) {
   store.user = null
+  $('#message').removeClass()
+  $('#message').addClass('success-message')
+  $('#message').show()
+  $('#message').text('User signed out of Practice Session Tracker.')
 }
 
 const onSignOutFailureTTT = function (response) {
-
+  $('#message').text('Error: user not signed out. Please try again.')
+  $('#message').removeClass()
+  $('#message').addClass('failure-message')
 }
 
 module.exports = {
