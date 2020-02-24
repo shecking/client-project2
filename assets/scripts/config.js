5d3686e91c21fba1ee6b2bfa1ef3dcb536476fd1
Steven Hecking <shecking1115@gmail.com>
2020-02-24 14:50:19 -0500

    Editing development URL

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,7 +3,7 @@
 let apiUrl
 const apiUrls = {
   production: 'https://rails-api-project2-production.herokuapp.com/',
-  development: 'http://rails-api-project2.herokuapp.com/'
+  development: 'localhost:4741'
 }
 
 if (window.location.hostname === 'localhost') {
